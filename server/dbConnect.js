850375dbed25e288ef11e1c9d0ac1a7dd2160425
Lokesh Goswami <lokesh.goswami.2003@gmail.com>
2025-12-16 20:13:09 +0530

    Project revival fixing issues fixing cloudinary and database

--- server/dbConnect.js
+++ server/dbConnect.js
@@ -1,16 +1,14 @@
 const mongoose = require("mongoose");
 
 module.exports = async () => {
-    const mongoUri =
-        "mongodb+srv://LokeshGoswami:fTZmR6eMhh4XuWv2@social-media.klur2u3.mongodb.net/?retryWrites=true&w=majority";
-    try {
-        const connect = await mongoose.connect(mongoUri, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
-        });
-        console.log("mongoDB Connected : ", connect.connection.host);
-    } catch (error) {
-        console.log(error);
-        process.exit(1);
-    }
+  try {
+    const connect = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Connected:", connect.connection.host);
+  } catch (error) {
+    console.error("MongoDB connection failed", error);
+    process.exit(1);
+  }
 };
--- server/index.js
+++ server/index.js
@@ -12,19 +12,19 @@ const cloudinary = require("cloudinary").v2;
 dotenv.config({ path: "./config.env" });
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const app = express();
 
 //middlewares
 app.use(
-    cors({
-        origin: "http://localhost:3000",
-        credentials: true,
-    })
+  cors({
+    origin: "http://localhost:3001",
+    credentials: true,
+  })
 );
 app.use(express.json({ limit: "20mb" }));
 app.use(morgan("common"));
@@ -36,11 +36,11 @@ app.use("/posts", postsRouter);
 app.use("/user", userRouter);
 
 app.get("/", (req, res) => {
-    res.status(200).json({ message: "hi" });
+  res.status(200).json({ message: "hi" });
 });
 
 const PORT = process.env.PORT || 4000;
 dbConnect();
 app.listen(PORT, () => {
-    console.log("Server Started on PORT ", PORT);
+  console.log("Server Started on PORT ", PORT);
 });
